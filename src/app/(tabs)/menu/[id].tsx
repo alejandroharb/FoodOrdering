00f38ed7c8fba3e8c7b6e11a1f786840bee7170d
alejandroharb <alejandroharb@gmail.com>
2024-07-15 10:33:53 -0500

    shopping cart

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,81 +1,106 @@
-import { useState } from 'react';
-import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
-import { Stack, useLocalSearchParams } from 'expo-router';
-import products from '@assets/data/products';
-import { defaultPizzaImage } from '@/components/ProductListItem';
-import Button from '@/components/Button';
+import { useState } from "react";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import Button from "@/components/Button";
+import { useCart } from "@providers/CartProvider";
+import { PizzaSize } from "@assets/types";
 
-const sizes = [ 'S', 'M', 'L', 'XL'];
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
-    const { id } = useLocalSearchParams();
-    const [selectedSize, setSelectedSize] = useState('M');
-    const addToCart = () => {
-        console.warn('adding to cart, size: ', selectedSize);
-    };
+  const { id } = useLocalSearchParams();
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
 
-    const product = products.find((p) => p.id.toString() === id);
+  const router = useRouter();
 
-    if(!product){
-        return <Text>Product Not Found</Text>
-    };
-    
-    return(
-        <View style={styles.container}>
-            <Stack.Screen 
-                options={{ title: product.name }} 
-            />
-            <Image 
-                source={{ uri: product.image || defaultPizzaImage }} 
-                style={styles.image} 
-            />
-            
-            <Text>Select size</Text>
-            <View style={styles.sizes}>
-                {sizes.map((size) => (
-                    <Pressable onPress={() => setSelectedSize(size)} style={[styles.size, { backgroundColor: selectedSize === size ? 'gainsboro' : 'white'}]} key={size}>
-                        <Text style={[styles.sizeText, { color: selectedSize === size ? 'black' : 'gray' }]}>{size}</Text>
-                    </Pressable>
-                ))}
-            </View>
-            <Text style={styles.price}>${product.price}</Text>
-            <Button onPress={addToCart} text="Add to cart"/>
-        </View>
-    )
+  const { addItem } = useCart();
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+    addItem(product, selectedSize);
+
+    router.push('/cart');
+  };
+
+  const product = products.find((p) => p.id.toString() === id);
+
+  if (!product) {
+    return <Text>Product Not Found</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
+
+      <Text>Select size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <Pressable
+            onPress={() => setSelectedSize(size)}
+            style={[
+              styles.size,
+              {
+                backgroundColor: selectedSize === size ? "gainsboro" : "white",
+              },
+            ]}
+            key={size}
+          >
+            <Text
+              style={[
+                styles.sizeText,
+                { color: selectedSize === size ? "black" : "gray" },
+              ]}
+            >
+              {size}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+      <Text style={styles.price}>${product.price}</Text>
+      <Button onPress={addToCart} text="Add to cart" />
+    </View>
+  );
 };
 
 export default ProductDetailsScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'white',
-        flex: 1,
-        padding: 10
-    },
-    image: {
-        width: '100%',
-        aspectRatio: 1
-    },
-    price: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginTop: 'auto'
-    },
-    sizes:{
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        marginVertical: 10
-    },
-    size: {
-      backgroundColor: 'gainsboro',
-      width: 50,
-      aspectRatio: 1,
-      borderRadius: 25,
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    sizeText: {
-        fontSize: 20,
-        fontWeight: '500'
-    }
-})
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: "auto",
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+});
